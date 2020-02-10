17054586833cf4e24af67f57e7ea34036e26a477
Michael Henry <michaeljhenry@hotmail.ca>
2020-02-10 03:19:46 -0500

    my project

---
+++ app.js
@@ -0,0 +1,48 @@
+var colourController = (function() {
+    return {
+        colourId: function() {
+            var colour = event.target.id.split("_")[0];
+            return colour;
+        }
+    }
+})();
+
+var UIController = (function() {
+    return {
+        addColour: function(colour) {
+            document.getElementById(event.target.id).style.backgroundColor = colour;
+
+        }
+    }
+})();
+
+var contorller = (function(colourCtrl, UICtrl) {
+
+    
+    var setupEventListeners = function() {
+        document.querySelectorAll(".btn").forEach(function(el) { // need to add eventListener to each button in the 'class'
+            el.addEventListener("click", colourIdCtrl)
+        });
+        document.querySelectorAll(".box").forEach(function(el) {
+            el.addEventListener("click", function() {
+                addColourCtrl(window.colour)});
+        });
+    }
+
+    var colourIdCtrl = function() {
+        window.colour =  colourCtrl.colourId();
+                     
+    }
+
+    var addColourCtrl = function(colour) {
+      
+        //console.log(colourIdCtrl);
+        UICtrl.addColour(colour);
+    }
+
+
+
+    setupEventListeners();
+
+
+})(colourController, UIController);
